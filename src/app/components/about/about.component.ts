2134a53a1c4580fb2a8697073df3b8cb0ad7e65d
Pritesh2004 <pmpatilpritesh123@gmail.com>
2024-01-08 22:02:14 +0530

    web update 3

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -27,19 +27,94 @@ export class AboutComponent  implements OnInit{
   }
 
 
-  openDialog(){
-    this.dialog.open(DialogAnimationsExampleDialog);
+  openExperienceDialog(){
+    this.dialog.open(DialogForExperience);
+  }
+
+  openEducationDialog(){
+    this.dialog.open(DialogForEducation);
+  }
+
+  openCertificateDialog(){
+    this.dialog.open(DialogForCertificate);
+  }
+
+  openProjectDialog(){
+    this.dialog.open(DialogForProject);
+  }
+
+  openProject1Dialog(){
+    this.dialog.open(DialogForProject1);
+  }
+
+  openProject2Dialog(){
+    this.dialog.open(DialogForProject2);
   }
 }
 
 
 
 @Component({
-  selector: 'extra-info',
-  templateUrl: 'extra-info.html',
+  selector: 'exp-dialog',
+  templateUrl: 'exp-dialog.html',
   standalone: true,
   imports: [MatButtonModule]
 })
-export class DialogAnimationsExampleDialog {
+export class DialogForExperience {
+  constructor() {}
+}
+
+
+@Component({
+  selector: 'edu-dialog',
+  templateUrl: 'edu-dialog.html',
+  standalone: true,
+  imports: [MatButtonModule]
+})
+export class DialogForEducation {
+  constructor() {}
+}
+
+
+@Component({
+  selector: 'certificate-dialog',
+  templateUrl: 'certificate-dialog.html',
+  standalone: true,
+  imports: [MatButtonModule]
+})
+export class DialogForCertificate {
+  constructor() {}
+}
+
+
+@Component({
+  selector: 'project-dialog',
+  templateUrl: 'project-dialog.html',
+  standalone: true,
+  imports: [MatButtonModule]
+})
+export class DialogForProject {
+  constructor() {}
+}
+
+
+@Component({
+  selector: 'pro1-dialog',
+  templateUrl: 'pro1-dialog.html',
+  standalone: true,
+  imports: [MatButtonModule]
+})
+export class DialogForProject1 {
+  constructor() {}
+}
+
+
+@Component({
+  selector: 'pro2-dialog',
+  templateUrl: 'pro2-dialog.html',
+  standalone: true,
+  imports: [MatButtonModule]
+})
+export class DialogForProject2 {
   constructor() {}
 }
