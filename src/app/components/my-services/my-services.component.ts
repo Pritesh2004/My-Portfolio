aedfc3dc22fed31c3d6a227afbc95bc9546a7da9
Pritesh2004 <pmpatilpritesh123@gmail.com>
2024-01-14 12:58:50 +0530

    web update 7

--- src/app/components/my-services/my-services.component.ts
+++ src/app/components/my-services/my-services.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my-services',
   templateUrl: './my-services.component.html',
-  styleUrls: ['./my-services.component.css']
+  styleUrls: ['./my-services.component.css','./service.css']
 })
 export class MyServicesComponent {
 
